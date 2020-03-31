40023eaeced15e9d08ab19f44f024526859a03f5
Mattéo DA SILVA <21804260@etu.unicaen.fr>
2020-04-01 00:31:06 +0200

    encore...

---
+++ app/models/project.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+const {Model} = DS;
+
+export default Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr('string'),
+  startDate: DS.attr('utc'),
+  dueDate: DS.attr('utc'),
+  owner: DS.belongsTo('developer'),
+  stories: DS.hasMany('story')
+});
