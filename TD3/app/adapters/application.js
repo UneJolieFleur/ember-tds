bdc397553df9ceab60c16fbcde0e4110e9cbeb4b
Mattéo DA SILVA <21804260@etu.unicaen.fr>
2020-03-31 18:11:06 +0200

    creation branche evaluation

--- TD3/app/adapters/application.js
+++
@@ -1,5 +0,0 @@
-import Adapter from 'ember-local-storage/adapters/local';
-
-export default Adapter.extend({
-  modelNamespace: 'contact-app'
-});
--- TD3/app/models/contact.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  nom:DS.attr('string'),
-  prenom:DS.attr('string'),
-  email:DS.attr('string')
-});
